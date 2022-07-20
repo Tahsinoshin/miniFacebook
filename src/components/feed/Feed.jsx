b7ba4b80dffeaf098f3879bec8f68a04053288b2
Tahsinoshin <48786946+Tahsinoshin@users.noreply.github.com>
2022-07-21 02:03:54 +0600

    Add files via upload

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,20 @@
 import "./feed.css";
+import Share from "../../components/share/Share";
+import Post from "../../components/post/Post"
 
 
 export default function Feed(){
     return(
         <div className="feed">
-           <div className="feedWrapper">this is Feed</div>
+            <div className="feedWrapper">
+                <Share/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+            
+            </div>
+            
         </div>
     )
 }
