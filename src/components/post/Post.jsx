345bb5faf5be6c973e1a6356e29666283ce9498f
Tahsinoshin <bsse1107@iit.du.ac.bd>
2022-07-22 01:48:10 +0600

    what a lode of crap shit

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,42 +1,20 @@
 import "./post.css"
 import {MoreVert} from "@mui/icons-material"
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-export default function Post() {
-=======
-=======
->>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
 import { Users } from "../../dummyData";
 
 export default function Post({post}) {
 
     const user= Users.filter(u=>u.id===1)
 
-<<<<<<< HEAD
->>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
-=======
->>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
   return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    <img className="postProfileImg" src="/assets/index.png" alt="" />
-                    <span className="postUsername">USERRRR</span>
-                    <span className="postDate">5 min ago</span>
-=======
                     <img className="postProfileImg" src={Users.filter(u=>u.id===post.userId)[0].profilePicture} alt="" />
                     <span className="postUsername">{Users.filter(u=>u.id===post.userId)[0].username}</span>
                     <span className="postDate">{post.date}</span>
->>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
-=======
-                    <img className="postProfileImg" src={Users.filter(u=>u.id===post.userId)[0].profilePicture} alt="" />
-                    <span className="postUsername">{Users.filter(u=>u.id===post.userId)[0].username}</span>
-                    <span className="postDate">{post.date}</span>
->>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
 
 
                 </div>
@@ -46,15 +24,7 @@ export default function Post({post}) {
 
             </div>
             <div className="postCenter">
-<<<<<<< HEAD
-<<<<<<< HEAD
-                <span className="postText">hey its my first post</span>
-=======
                 <span className="postText">{post.desc}</span>
->>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
-=======
-                <span className="postText">{post.desc}</span>
->>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
                 <img className="postImg" src="assets/images.png"  alt=""/>
             </div>
             <div className="postBottom">
@@ -66,5 +36,5 @@ export default function Post({post}) {
         </div>
       
     </div>
-  )
+  );
 }
