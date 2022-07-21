8e5639d5af3754052210b6b1d3bef25fd10c7288
Tahsinoshin <bsse1107@iit.du.ac.bd>
2022-07-22 01:00:21 +0600

    locally giving

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,6 +1,10 @@
 import "./feed.css";
 import Share from "../../components/share/Share";
 import Post from "../../components/post/Post"
+<<<<<<< HEAD
+=======
+import {Posts} from "../../dummyData";
+>>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
 
 
 export default function Feed(){
@@ -8,10 +12,18 @@ export default function Feed(){
         <div className="feed">
             <div className="feedWrapper">
                 <Share/>
+<<<<<<< HEAD
                 <Post/>
                 <Post/>
                 <Post/>
                 <Post/>
+=======
+                {Posts.map(p=>(
+                     <Post key={p.id} post={p}/>
+
+                ))}
+               
+>>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
             
             </div>
             
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,30 @@
 import "./post.css"
 import {MoreVert} from "@mui/icons-material"
+<<<<<<< HEAD
 
 export default function Post() {
+=======
+import { Users } from "../../dummyData";
+
+export default function Post({post}) {
+
+    const user= Users.filter(u=>u.id===1)
+
+>>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
   return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
+<<<<<<< HEAD
                     <img className="postProfileImg" src="/assets/index.png" alt="" />
                     <span className="postUsername">USERRRR</span>
                     <span className="postDate">5 min ago</span>
+=======
+                    <img className="postProfileImg" src={Users.filter(u=>u.id===post.userId)[0].profilePicture} alt="" />
+                    <span className="postUsername">{Users.filter(u=>u.id===post.userId)[0].username}</span>
+                    <span className="postDate">{post.date}</span>
+>>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
 
 
                 </div>
@@ -19,7 +34,11 @@ export default function Post() {
 
             </div>
             <div className="postCenter">
+<<<<<<< HEAD
                 <span className="postText">hey its my first post</span>
+=======
+                <span className="postText">{post.desc}</span>
+>>>>>>> 9362f5e8d0625408435e3913809f78f229fefcc2
                 <img className="postImg" src="assets/images.png"  alt=""/>
             </div>
             <div className="postBottom">
